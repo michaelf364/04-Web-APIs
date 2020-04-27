af4b5cd08863d97735a52fc31c9a7e084941f1e7
Michael Farris <farris.michael.j@gmail.com>
2020-04-26 23:53:04 -0700

    finished code?

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,61 +1,248 @@
-var quizQuestion = document.querySelector("#quizQuestion");
-var quizAnswer = document.querySelector("#quizAnswer");
-var minutesDisplay = document.querySelector("#minutes");
-var secondsDisplay = document.querySelector("#seconds");
-var time = 60;
+//start screen
+var startScn = document.getElementById("startScn");
+var startBtn = document.getElementById("startBtn");
+var highScoresBtn = document.getElementById("highScoresBtn");
+
+//question screen
+var questionScn = document.getElementById("questionScn");
+var timeLeft = document.getElementById("timeLeft");
+var currentQuestion = document.getElementById("currentQuestion");
+var answers = document.getElementById("answers");
+var results = document.getElementById("results");
+
+//victory screen
+var victoryScn = document.getElementById("victoryScn");
+var nameInput = document.getElementById("nameInput");
+var score = document.getElementById("score");
+var playAgainVSBtn = document.getElementById("playAgainVSBtn");
+var highScoresVSBtn = document.getElementById("highScoresVSBtn");
+
+//defeat screen
+var defeatScn = document.getElementById("defeatScn");
+var playAgainDSBtn = document.getElementById("playAgainDSBtn");
+var highScoresDSBtn = document.getElementById("highScoresDSBtn");
+
+//high score screen
+var highScoreScn = document.getElementById("highScoreScn");
+var highScores = document.getElementById("highScores");
+var backButton = document.getElementById("backButton");
+
+var currentIndex;
+var timeLeft;
+var timer;
+var highScores = [
+    {
+        name: "None",
+        score: 0
+    },
+    {
+        name: "None",
+        score: 0
+    },
+    {
+        name: "None",
+        score: 0
+    },
+    {
+        name: "None",
+        score: 0
+    },
+    {
+        name: "None",
+        score: 0
+    }
+];
 var questions =
     [
         {
             question: "Commonly used data types DO NOT include",
             choices: ["strings", "booleans", "alerts", "numbers"],
-            answer: 2
+            correctAnswer: 2
         },
-
         {
             question: "The condition in an if / else statement is enclosed within ____.",
             choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-            answer: 2
+            correctAnswer: 2
         },
-
         {
             question: "String values must be enclosed within ____ when being assigned to variables.",
             choices: ["commas", "curly brackets", "quotes", "parentheses"],
-            answer: 2
+            correctAnswer: 2
         },
         {
             question: "Arrays in JavaScript can be used to store ____.",
             choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-            answer: 3
+            correctAnswer: 3
         },
         {
             question: "A very useful tool used during development and debugging for printing content to the debugger is",
             choices: ["Javascript", "terminal", "for loops", "console.log"],
-            answer: 3
+            correctAnswer: 3
         }
     ];
-    window.onload = function(){
+initialize();
+function initialize() {
+    loadScores();
+    currentIndex = 0;
+    timeLeft = 60;
+    startScn.style.display = "block";
+    questionScn.style.display = "none";
+    victoryScn.style.display = "none";
+    defeatScn.style.display = "none";
+    highScoreScn.style.display = "none";
+}
 
+startBtn.addEventListener("click", function () {
+    timeLeft = 60;
+    currentIndex = 0;
+    startScn.style.display = "none";
+    questionScn.style.display = "block";
+    victoryScn.style.display = "none";
+    defeatScn.style.display = "none";
+    highScoreScn.style.display = "none";
+    ShowQuestionScn(currentIndex);
+    startTimer();
+    timeLeftSpan.textContent = timeLeft;
+});
+
+highScoresBtn.addEventListener("click", function () {
+    renderHighScores();
+    displayHighScoreScreen();
+});
+highScoresVSBtn.addEventListener("click", function () {
+    renderHighScores();
+    displayHighScoreScreen();
+});
+highScoresDSBtn.addEventListener("click", function () {
+    renderHighScores();
+    displayHighScoreScreen();
+});
+
+function ShowQuestionScn(index) {
+    answers.innerHTML = "";
+    currentQuestion.textContent = questions[index].question;
+    for (var i = 0; i < questions[index].choices.length; i++) {
+        var question = document.createElement("li");
+        var questionBtn = document.createElement("button");
+        questionBtn.setAttribute("type", "button");
+        questionBtn.setAttribute("id", i);
+        questionBtn.textContent = questions[index].choices[i];
+        questionBtn.setAttribute("class", "btn btn-primary");
+        question.appendChild(questionBtn);
+        answers.appendChild(question);
     }
-function startQuiz() {
-    var quizTime = setInterval(() => {
-        if (time === undefined) {
-            clearInterval(quizTime);
-        } else {
-            Quiz();
+}
+
+answers.addEventListener("click", function (playerAnswer) {
+    if (playerAnswer.target.matches("button")) {
+        var answer = playerAnswer.target.getAttribute("id");
+        var isCorrect = checkAnswer(currentIndex, answer);
+        if (isCorrect === true) {
+            results.textContent = "Correct";
         }
-    }, 1000);
-}
-function Quiz() {
-
-for (let i = 0; i < array.length; i++) {
-
-    if (questions) {
-        //answer wrong
-        time--;
-    }else{
-        //answer correct
+        else {
+            results.textContent = "Incorrect";
+            timeLeft -= 10;
+            timeLeftSpan.textContent = timeLeft;
+        }
+        setTimeout(clearResults, 1500)
+        currentIndex++;
+        if (currentIndex === questions.length) {
+            clearInterval(timer);
+            displayVictoryScreen();
+            checkScore();
+            return;
+        }
+        ShowQuestionScn(currentIndex);
     }
-    
-}
+});
+
+backBtn.addEventListener("click", function () {
+    initialize();
+});
+
+function checkAnswer(index, answer) {
+    var userAnswer = parseInt(answer);
+    var correctAnswer = parseInt(questions[index].correctAnswer)
+    var isCorrect = false;
+    if (userAnswer === correctAnswer) {
+        isCorrect = true;
+    }
+    return isCorrect;
 }
 
+function clearResults() {
+    results.textContent = "";
+}
+
+function startTimer() {
+    timer = setInterval(function () {
+        timeLeft--;
+        if (timeLeft < 0) {
+            DisplayDefeatScreen();
+            clearInterval(timer);
+        }
+        timeLeftSpan.textContent = timeLeft;
+    }, 1000)
+}
+
+function DisplayDefeatScreen(){
+    questionScn.style.display = "none";
+    startScn.style.display = "none";
+    victoryScn.style.display = "none";
+    defeatScn.style.display = "block";
+    highScoreScn.style.display = "none";
+    score.textContent = timeLeft;
+}
+function displayVictoryScreen() {
+    questionScn.style.display = "none";
+    startScn.style.display = "none";
+    victoryScn.style.display = "block";
+    highScoreScn.style.display = "none";
+    defeatScn.style.display = "none";
+    score.textContent = timeLeft;
+}
+
+function displayHighScoreScreen() {
+    startScn.style.display = "none";
+    questionScn.style.display = "none";
+    victoryScn.style.display = "none";
+    defeatScn.style.display = "none";
+    highScoreScn.style.display = "block";
+}
+function checkScore() {
+    var highScore = {
+        name: "",
+        score: timeLeft
+    };
+
+    for (var i = 0; i < highScores.length; i++) {
+        if (timeLeft > parseInt(highScores[i].score)) {
+            highScore.name = nameInput.textContent;
+            highScores.splice(i, 0, highScore);
+            highScores.splice(5, 1);
+            saveScores();
+            i = highScores.length;
+        }
+    }
+}
+
+function renderHighScores() {
+    highScoresList.innerHTML = "";
+    for (var i = 0; i < highScores.length; i++) {
+        var score = document.createElement("li");
+        score.textContent = ((i + 1) + ".  " + highScores[i].name + ":  " + highScores[i].score);
+        highScoresList.appendChild(score);
+    }
+}
+
+function saveScores() {
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+function loadScores() {
+    var tempScores = JSON.parse(localStorage.getItem("highScores"));
+    if (tempScores !== null) {
+        highScores = tempScores;
+    }
+}
--- README.md
+++ README.md
@@ -1,42 +1,23 @@
-# 04 Web APIs: Code Quiz
+## Table of Contents
 
-As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+[Javascript Quiz](#Javascript-Quiz)
 
-## User Story
+[Technology-Used](#Technology-Used)
 
-```
-AS A coding bootcamp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
+[Screenshots](#Screenshots)
 
-## Acceptance Criteria
+# Javascript Quiz
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-```
+This is a responsive website built to host a javascript quiz
 
-The following animation demonstrates the application functionality:
+### Technology Used
 
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
+* HTML
+* CSS
+* themed bootstrap from [bootswatch](https://bootswatch.com/slate/)
+* Javascript
 
-### Review
-
-You are required to submit the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+### Screenshots
+|Small|Medium|Large|
+|---|---|---|
+| ![640-password](./Assets/images/640-password.jpg) | ![768-password](./Assets/images/768-password.jpg) | ![940-password](./Assets/images/940-password.jpg) |
